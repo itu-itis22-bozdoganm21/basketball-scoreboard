8556f6c63026a48e8783846fbb9169a7b22a3ae1
itu-itis22-bozdoganm21 <bozdoganm21@itu.edu.tr>
2022-11-04 15:10:34 +0300

    I uploaded my basketball scoreboard

---
+++ index.js
@@ -0,0 +1,33 @@
+let home  = document.getElementById("skor")
+let guest = document.getElementById("skor2")
+
+function arti1(){
+    
+    home.innerText = parseInt(home.innerText) + 1
+    
+}
+function arti2(){
+    
+    home.innerText = parseInt(home.innerText) + 2
+    
+}
+function arti3(){
+    
+    home.innerText = parseInt(home.innerText) + 3
+    
+}
+function artibir(){
+    
+    guest.innerText = parseInt(guest.innerText) + 1
+    
+}
+function artiiki(){
+    
+    guest.innerText = parseInt(guest.innerText) + 2
+    
+}
+function artiuc(){
+    
+    guest.innerText = parseInt(guest.innerText) + 3
+    
+}
